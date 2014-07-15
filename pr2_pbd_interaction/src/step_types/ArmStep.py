75f04b00d095493fecc9f955dc12842da43d352e
Sonya Alexandrova <sonya.alexandrova@gmail.com>
2014-07-15 13:27:04 -0700

    arm step can only have gripper condition (dubious, need to discuss)

--- pr2_pbd_interaction/src/step_types/ArmStep.py
+++ pr2_pbd_interaction/src/step_types/ArmStep.py
@@ -18,7 +18,7 @@ class ArmStep(Step):
     ARM_TRAJECTORY = 1
     def __init__(self, *args, **kwargs):
         Step.__init__(self, *args, **kwargs)
-        self.conditions.append(GripperCondition())
+        self.conditions = [GripperCondition()]
         self.condition_order = xrange(len(self.conditions))
         self.type = ArmStep.ARM_TARGET
         self.armTarget = None
