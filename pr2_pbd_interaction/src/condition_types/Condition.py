f76b96b095077a7041201de0385709890fe5d2b6
Sonya Alexandrova <sonya.alexandrova@gmail.com>
2014-07-17 14:15:54 -0700

    don't really need ids for conditions

--- pr2_pbd_interaction/src/condition_types/Condition.py
+++ pr2_pbd_interaction/src/condition_types/Condition.py
@@ -7,7 +7,6 @@ class Condition:
     def __init__(self, *args, **kwargs):
         self.available_strategies = [Strategy.FAIL_FAST]
         self.current_strategy_index = 0
-        self.id = uuid.uuid4()
 
     def check(self):
         """ Returns True if condition is satisfied, False otherwise.
